055a19e64cede4184bc824e5cf04eaaa07b11814
Jean Desravines <jeandesravines@users.noreply.github.com>
2017-04-09 13:55:06 +0200

    Chore indent (#26)
    * Init
    
    * Update Readme file
    
    * Configure Travis CI
    
    * Coveralls + Readme's badges
    
    * Merge
    
    * .gitignore
    
    * delete npm-debug.log
    
    * Configure Pre-Commit
    
    * Update .gitgnore
    
    * package.json
    
    * package.json
    
    * Travis + Coveralls
    
    * package.json
    
    * NPM scope
    
    * README
    
    * version
    
    * Improve testing
    
    * Package.json
    
    * filenames
    
    * Test
    
    - Improve tests
    
    * Tests
    
    * README
    
    * version
    
    * Config
    
    * package.json
    
    * package.json
    
    * package.json
    
    * feat: package.json
    
    * doc(README): remove $
    
    * fix: package.json
    
    * feat: linter + hooks
    
    * feat: eslint + travis
    
    * chore: indent
    
    * chore: copyright
    
    * feat: remove sudo

--- test/unit/helper/mutex.js
+++ test/unit/helper/mutex.js
@@ -1,71 +1,67 @@
-'use strict';
-
 /**
- * Copyright 2016 Jean Desravines <hi@jeandesravines.com>
+ * Copyright 2017 Jean Desravines <hi@jeandesravines.com>
  */
 
-const {afterEach, beforeEach, describe, it} = require('mocha');
+'use strict';
+
+const {beforeEach, describe, it} = require('mocha');
 const {expect} = require('chai');
 const Mutex = require('../../../lib/helper/mutex');
 
 describe('Mutex', () => {
-	/**
-	 * @type {Mutex}
-	 */
-	let mutex;
+  /**
+   * @type {Mutex}
+   */
+  let mutex;
 
-	beforeEach('Instantiate', () => {
-		mutex = new Mutex();
-	});
+  beforeEach('Instantiate', () => {
+    mutex = new Mutex();
+  });
 
-	afterEach('Destroy', () => {
-		mutex = null;
-	});
+  describe('Synchronous', () => {
+    it('should lock', () => {
+      expect(mutex.lock()).to.be.equal(true);
+      expect(mutex.locked).to.be.equal(true);
+    });
+  });
 
-	describe('Synchronous', () => {
-		it('should lock', () => {
-			expect(mutex.lock()).to.equal(true);
-			expect(mutex.locked).to.equal(true);
-		});
-	});
+  describe('Asynchronous', () => {
+    it('should lock and unlock', () => {
+      return mutex.asynchronous()
+        .then(() => mutex.unlock())
+        .then(() => {
+          expect(mutex.locked).to.be.equal(false);
+        });
+    });
 
-	describe('Asynchronous', () => {
-		it('should lock and unlock', () => {
-			return mutex.asynchronous()
-				.then(() => mutex.unlock())
-				.then(() => {
-					expect(mutex.locked).to.equal(false);
-				});
-		});
+    it('should reject the lock', () => {
+      return mutex.asynchronous()
+        .then(() => mutex.asynchronous())
+        .then(() => Promise.reject())
+        .catch(() => Promise.resolve());
+    });
+  });
 
-		it('should reject the lock', () => {
-			return mutex.asynchronous()
-				.then(() => mutex.asynchronous())
-				.then(() => Promise.reject())
-				.catch(() => Promise.resolve());
-		});
-	});
+  describe('Delayed', () => {
+    it('should lock', () => {
+      return mutex.delayed(500);
+    });
 
-	describe('Delayed', () => {
-		it('should lock', () => {
-			return mutex.delayed(500);
-		});
+    it('should reject the lock', () => {
+      return mutex.delayed(500)
+        .then(() => mutex.delayed(500))
+        .then(() => Promise.reject())
+        .catch(() => Promise.resolve());
+    });
 
-		it('should reject the lock', () => {
-			return mutex.delayed(500)
-				.then(() => mutex.delayed(500))
-				.then(() => Promise.reject())
-				.catch(() => Promise.resolve());
-		});
-
-		it('should be unlocked', () => {
-			return mutex.delayed(500)
-				.then(() => new Promise((resolve, reject) => {
-					setTimeout(() => {
-						expect(mutex.locked).to.equal(false);
-						resolve();
-					}, 500);
-				}));
-		});
-	});
+    it('should be unlocked', () => {
+      return mutex.delayed(500)
+        .then(() => new Promise((resolve) => {
+          setTimeout(() => {
+            expect(mutex.locked).to.be.equal(false);
+            resolve();
+          }, 500);
+        }));
+    });
+  });
 });
